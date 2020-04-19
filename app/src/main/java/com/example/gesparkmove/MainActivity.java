3a2ea7055646463083e1f8fc5fbbed69d975b4e2
Nuno Miguel Cunha Francisco <nunomcfrancisco@gmail.com>
2020-04-19 15:09:56 +0100

    Drawer Menu 2 of 4 done!

--- app/src/main/java/com/example/gesparkmove/MainActivity.java
+++ app/src/main/java/com/example/gesparkmove/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.gesparkmove;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, UtilizadorActivity.class));
+                finish();
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/gesparkmove/UtilizadorActivity.java
+++ app/src/main/java/com/example/gesparkmove/UtilizadorActivity.java
@@ -1,14 +1,32 @@
 package com.example.gesparkmove;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class UtilizadorActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_utilizador);
+        toolbar = findViewById(R.id.drawertoolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
     }
 }
