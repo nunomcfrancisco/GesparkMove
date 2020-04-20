c3a4c03280e0ed48a19ce2f0c1b585b9080a6abf
Nuno Miguel Cunha Francisco <nunomcfrancisco@gmail.com>
2020-04-20 09:11:53 +0100

    Drawer Menu 3 of 4 done!

--- app/src/main/java/com/example/gesparkmove/UtilizadorActivity.java
+++ app/src/main/java/com/example/gesparkmove/UtilizadorActivity.java
@@ -1,19 +1,25 @@
 package com.example.gesparkmove;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class UtilizadorActivity extends AppCompatActivity {
+public class UtilizadorActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
     NavigationView navigationView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +27,36 @@ public class UtilizadorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_utilizador);
         toolbar = findViewById(R.id.drawertoolbar);
         setSupportActionBar(toolbar);
+
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBarDrawerToggle.syncState();
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.containerFragment, new dashboardFragment());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menuItemAdicionar:
+            break;
+            case R.id.menuItemAlterar:
+            break;
+            case R.id.menuItemEstacionamentos:
+            break;
+            case R.id.menuItemInactivos:
+            break;
+            case R.id.menuItemPagamentos:
+            break;
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/gesparkmove/adicionarFragment.java
@@ -0,0 +1,6 @@
+package com.example.gesparkmove;
+
+import androidx.fragment.app.Fragment;
+
+public class adicionarFragment extends Fragment {
+}
