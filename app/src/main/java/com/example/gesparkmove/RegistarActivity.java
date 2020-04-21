79b961e7d3867fab8f8fa4240e51705ff9490070
Nuno Miguel Cunha Francisco <nunomcfrancisco@gmail.com>
2020-04-21 10:59:27 +0100

    Teste de ligação SSH e Base de Dados efetuado com sucesso!

--- app/src/main/java/com/example/gesparkmove/RegistarActivity.java
+++ app/src/main/java/com/example/gesparkmove/RegistarActivity.java
@@ -9,25 +9,29 @@ import android.widget.Button;
 import android.widget.TextView;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
+import com.jcraft.jsch.*;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class RegistarActivity extends AppCompatActivity {
 
     TextView text, errorText;
     Button show;
+    Button query;
+    String records = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registar);
 
-
-
-        text = (TextView) findViewById(R.id.textViewTesteDB);
-        errorText = (TextView) findViewById(R.id.textViewNoError);
-        show = (Button) findViewById(R.id.buttonShowRecords);
+        text = findViewById(R.id.textViewTesteDB);
+        errorText = findViewById(R.id.textViewNoError);
+        show = findViewById(R.id.buttonShowRecords);
+        query = findViewById(R.id.buttonBD);
 
         show.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,29 +42,42 @@ public class RegistarActivity extends AppCompatActivity {
     }
 
     class Task extends AsyncTask<Void, Void, Void>{
-        String records = "", error = "";
+        String error = "";
         @Override
         protected Void doInBackground(Void... voids){
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://92.222.70.24:22/cgs_gespark", "gremote", "GespPW01");
-                Statement statement = (Statement) connection.createStatement();
-//                ResultSet resultSet = statement.executeQuery("SELECT * FROM utilizadores LIMIT 1");
-
-//                while (resultSet.next()){
-//                    records += resultSet.getInt(1);
-//                }
+                JSch jsch = new JSch();
+                Session session = jsch.getSession("gpark", "92.222.70.24", 22);
+                session.setPassword("GespPW01");
+                Properties prop = new Properties();
+                prop.put("StrictHostKeyChecking", "no");
+                session.setConfig(prop);
+                session.connect();
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://92.222.70.24:3306/cgs_gespark", "gremote", "GespPW01");
+                    Statement statement = (Statement) connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery("SELECT * FROM utilizadores LIMIT 1");
+                    while (resultSet.next()){
+                        records += " " + resultSet.getString(3);
+                    }
+                    connection.close();
+                } catch (SQLException | ClassNotFoundException e){}
             }
             catch (Exception e){
                 error = e.toString();
             }
+
             return null;
         }
         @Override
         protected void onPostExecute(Void aVoid){
-            text.setText(records);
-            if(error != "")
+            if(error != "") {
                 errorText.setText(error);
+            } else {
+                text.setText("Ligado!");
+                text.setText(records);
+            }
             super.onPostExecute(aVoid);
         }
     }
