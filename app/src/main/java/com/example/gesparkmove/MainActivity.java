7e886dae120d7d820b8ca04ba8d86e81030b97aa
Nuno Miguel Cunha Francisco <nunomcfrancisco@gmail.com>
2020-04-22 13:42:28 +0100

    Revert "Criado class sshTools"
    This reverts commit c7c781ba

--- app/src/main/java/com/example/gesparkmove/MainActivity.java
+++ app/src/main/java/com/example/gesparkmove/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
                 startActivity(new Intent(MainActivity.this, RegistarActivity.class));
+                finish();
             }
         });
     }
