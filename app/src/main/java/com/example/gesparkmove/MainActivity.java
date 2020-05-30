8ce1e4017d79fa34e1b52ae74bbcf80424604d51
Nuno Miguel Cunha Francisco <nunomcfrancisco@gmail.com>
2020-05-30 12:36:48 +0100

    Update da dataUltimoAcesso

--- app/src/main/java/com/example/gesparkmove/MainActivity.java
+++ app/src/main/java/com/example/gesparkmove/MainActivity.java
@@ -1,10 +1,5 @@
 package com.example.gesparkmove;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -17,15 +12,15 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
-import com.mysql.jdbc.Util;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -58,7 +53,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View view){
                 closeKeyboard();
-                new loginTask().execute("SELECT id, nif, nome, email, password, avatar FROM utilizadores WHERE email = \""
+                new loginTask().execute("SELECT id, nif, nome, email, password, avatar, activo FROM utilizadores WHERE email = \""
                         + editTextMainUtilizador.getText().toString() + "\"",
                         editTextMainPassword.getText().toString(),
                         "SELECT COUNT(matricula) FROM veiculos WHERE id_utilizador = (SELECT id FROM utilizadores WHERE email = \""
@@ -122,7 +117,7 @@ public class MainActivity extends AppCompatActivity{
                     Statement statement = (Statement) connection.createStatement();
                     ResultSet rs = statement.executeQuery(params[0]);
                     while (rs.next()){
-                        user = new Utilizador(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), 5, rs.getString(6),0);
+                        user = new Utilizador(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), 5, rs.getString(6), rs.getInt(7));
                         queryResult += rs.getString(5);
                     }
                     if(!queryResult.isEmpty()){
@@ -135,11 +130,33 @@ public class MainActivity extends AppCompatActivity{
                 }catch (ClassNotFoundException | SQLException e){}
                 session.disconnect();
             } catch (JSchException e){}
-            if(queryResult.equals("")){
+            if(queryResult.equals(""))
                 return "u";
-            }
+            else if(user.getAtivo() == 0)
+                return "a";
             else if(queryResult.equals(new md5Tools().encode(params[1]))){
-
+                try {
+                    JSch jsch = new JSch();
+                    Session session = jsch.getSession(g.getSshUsername(), g.getSshHost(), g.getSshPort());
+                    session.setPassword(g.getSshPass());
+                    session.setPortForwardingL(g.getSshPFLPort(), g.getSshPFHost(), g.getSshPFRPort());
+                    Properties prop = new Properties();
+                    prop.put("StrictHostKeyChecking", "no");
+                    session.setConfig(prop);
+                    session.connect();
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection connection = (Connection) DriverManager.getConnection(g.getMySqlUrl(), g.getMySqlUsername(), g.getMySqlPass());
+                        Statement statement = (Statement) connection.createStatement();
+                        statement.executeUpdate("UPDATE utilizadores SET dataUltimoAcesso = NOW() WHERE id = " + user.getId());
+                        connection.close();
+                    } catch (ClassNotFoundException | SQLException e) {
+                        e.printStackTrace();
+                    }
+                    session.disconnect();
+                } catch (JSchException e) {
+                    e.printStackTrace();
+                }
                 return "y";
             }else{
                 return "p";
@@ -161,8 +178,8 @@ public class MainActivity extends AppCompatActivity{
 
         @Override
         protected void onPostExecute(final String result){
-            if(result.equals("u")){
-                if(ppm.isShowing()) ppm.dismiss();
+            if(result.equals("u")) {
+                if (ppm.isShowing()) ppm.dismiss();
                 mainHandler.post(new Runnable() {
                     @Override
                     public void run() {
@@ -170,9 +187,27 @@ public class MainActivity extends AppCompatActivity{
                                 .setTitle("Erro!")
                                 .setMessage("Utilizador inválido!")
                                 .setCancelable(false)
-                                .setPositiveButton("Ok", new DialogInterface.OnClickListener(){
+                                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                                     @Override
-                                    public void onClick(DialogInterface dialog, int which) {}
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }
+                                })
+                                .show();
+                    }
+                });
+            }else if(result.equals("a")){
+                if (ppm.isShowing()) ppm.dismiss();
+                mainHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ppm = new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Erro!")
+                                .setMessage("Conta não ativa.")
+                                .setCancelable(false)
+                                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                    }
                                 })
                                 .show();
                     }
@@ -184,7 +219,7 @@ public class MainActivity extends AppCompatActivity{
                     public void run() {
                         ppm = new AlertDialog.Builder(MainActivity.this)
                                 .setTitle("Erro!")
-                                .setMessage(new md5Tools().encode("123"))
+                                .setMessage("Password inválida!")
                                 .setCancelable(false)
                                 .setPositiveButton("Ok", new DialogInterface.OnClickListener(){
                                     @Override
